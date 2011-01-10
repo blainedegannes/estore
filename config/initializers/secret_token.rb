dd888c5c0aa228b6a2e9c15c24f9e2ac57db640b
Blaine Degannes <blaine@entreelife.com>
2011-01-10 15:35:05 -0500

    Starting Over

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estore::Application.config.secret_token = '5d3fb52eaf75c780af729817fad6451195b41327c75fd32374986aaad2e0b323da498e7774e9a49519ed98985037f39734ea955bae10b0e032bb60d46cc42b89'
