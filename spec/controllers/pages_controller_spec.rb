795894052ab13da0340f83871ad4126d36f236ed
blainedegannes <blaine@entreelife.com>
2011-01-10 20:44:07 -0500

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+  
+  before(:each) do
+      	@base_title = "Entree"
+    end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+          get 'about'
+          response.should have_selector("title", :content => @base_title + " | About")
     end
   end
 
@@ -13,6 +23,11 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+          get 'contact'
+          response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
 
